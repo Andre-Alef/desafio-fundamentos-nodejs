9f9374807556896902592dc10b9af933590db262
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2021-03-11 20:19:20 -0300

    Criação das funções

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
+import { response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO{
+  title: string;
+  value:number;
+  type: 'income' | 'outcome'
+}
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+interface teste{
+  total1 : Array<number | undefined>;
 }
 
 class TransactionsRepository {
@@ -14,15 +25,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    
+      return this.transactions;
+
   }
 
   public getBalance(): Balance {
-    // TODO
+  
+    let income = 0
+    let outcome = 0
+    this.transactions.forEach(transaction => {
+    if(transaction.type == 'income'){
+      income += transaction.value
+    }   
+    else { 
+      outcome += transaction.value
+    }
+  }
+    )
+  
+    const balance: Balance = {
+      income:  income,
+      outcome: outcome,
+      total: income - outcome
+    }
+
+    return balance;
+
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type}: CreateTransactionDTO ): Transaction {
+    
+    let transaction = new Transaction({title, value, type})
+
+      this.transactions.push(transaction)
+  
+      return transaction
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public async  execute({ title, value, type}: Request): Promise<Transaction> {
     // TODO
+    const balance = await this.transactionsRepository.getBalance()
+    if(type == 'outcome' && balance.total < value) {
+        throw new Error('Saldo insuficiente')
+    }
+    const transaction= await this.transactionsRepository.create({
+       title,
+        value,
+         type})
+
+    return transaction;
   }
 }
 
